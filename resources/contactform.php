64b770de9d58cff9d5303200e6b99bb233b51cc2
Andy Chen <andychen@Andys-MacBook-Pro.local>
2018-06-29 17:27:18 -0700

    Added a contact form. Some cosmetic changes and code cleanup. PHP contact form template is in resources folder for future implementation.

---
+++ resources/contactform.php
@@ -0,0 +1,23 @@
+<div class="contactform">
+	<form action="contact_process.php">
+
+	    <!-- <label for="name">Name</label> -->
+	    <input type="text" id="name" name="fullname" placeholder="Your name">
+	    <br>
+	    <!-- <label for="email">Email</label> -->
+	    <input type="text" id="email" name="email" placeholder="Your email">
+	    <br>
+	    <!-- <label for="country">Country</label>
+	    <select id="country" name="country">
+	      <option value="australia">Australia</option>
+	      <option value="canada">Canada</option>
+	      <option value="usa">USA</option>
+	    </select> -->
+
+	    <!-- <label for="subject">Subject</label> -->
+	    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+	    <br>
+	    <input target="_blank" class="btn btn-success btn-lg" type="submit" value="Submit">
+
+	</form>
+</div>
